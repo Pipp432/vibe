c0fa9afd981dd5cf526837bf81d92130ee296b86
Pipp432 <pipp432@gmail.com>
2024-02-19 20:47:50 +0700

    Refactor login logic to useLogin hook
    Create useLogin to handle all login logic

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -8,44 +8,18 @@ import { useState } from "react";
 import LoadingPage from "@/components/loading/LoadingPage";
 import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
 import app from "@/firebase";
+import useLogin from "@/hooks/useLogin";
 export default function LoginPage() {
-  const auth = getAuth(app);
-  const router = useRouter();
-  const [username, setUsername] = useState("")
-  const [password, setPassword] = useState("")
-  const [isLoading, setIsLoading] = useState(false)
-  const [isError, setIsError] = useState(false)
-  const handleUserLogin = async (username: string, password: string) => {
-    try {
-      await signInWithEmailAndPassword(auth, username, password)
-
-      setIsLoading(false)
-
-      router.push("/educator/dashboard");
-    }
-    catch (e) {
-      setIsLoading(false)
-      setIsError(true)
-    }
-  }
-  const handleUsernameChange = (event: React.FormEvent<HTMLInputElement>) => {
-    setIsError(false)
-    setUsername(event.currentTarget.value)
-
-  }
-  const handlePasswordChange = (event: React.FormEvent<HTMLInputElement>) => {
-    setPassword(event.currentTarget.value)
-    setIsError(false)
-  }
+  const { loginFunction, loginState } = useLogin();
   const InputStyles = {
-    "normal":"p-2 w-[600px] h-[50px] rounded-2xl bg-background",
-    "error":"p-2 w-[600px] h-[50px] rounded-2xl bg-background border-red-500 border-4"
+    "normal": "p-2 w-[600px] h-[50px] rounded-2xl bg-background",
+    "error": "p-2 w-[600px] h-[50px] rounded-2xl bg-background border-red-500 border-4"
 
   }
   return (
     <>
-      {isLoading && <LoadingPage />}
-      {!isLoading && <div className='flex flex-row h-[100vh] justify-between'>
+      {loginState.isLoading && <LoadingPage />}
+      {!loginState.isLoading && <div className='flex flex-row h-[100vh] justify-between'>
         <div className='flex items-center justify-items-center flex-row ml-[20%] '>
           <Image src={logo} width={300} height={300} alt='Vibe logo' />
         </div>
@@ -53,15 +27,16 @@ export default function LoginPage() {
         <div className='bg-primary h-full w-[40%]  p-10'>
           <div className='flex mt-28 flex-col gap-16'>
             <LoginInput isPassword={false} title='Username'
-              onChangeHandler={handleUsernameChange} style={isError?InputStyles.error:InputStyles.normal}/>
+              onChangeHandler={loginFunction.handleUsernameChange}
+              style={loginState.isError ? InputStyles.error : InputStyles.normal} />
             <div>
-              <LoginInput isPassword={true} title='Password' onChangeHandler={handlePasswordChange} 
-                style={isError?InputStyles.error:InputStyles.normal} />
-              {!isError && <div className='flex flex-row-reverse mt-4'>
+              <LoginInput isPassword={true} title='Password' onChangeHandler={loginFunction.handlePasswordChange}
+                style={loginState.isError ? InputStyles.error : InputStyles.normal} />
+              {!loginState.isError && <div className='flex flex-row-reverse mt-4'>
                 <div className='text-white cursor-pointer'>{"Forgot Password?"}</div>
               </div>}
 
-              {isError && <div className="flex flex-row gap-48 mt-4">
+              {loginState.isError && <div className="flex flex-row gap-48 mt-4">
                 <div className="text-red-500">{"Invalid Username or Password"}</div>
 
                 <div className='text-white cursor-pointer'>{"Forgot Password?"}</div>
@@ -70,11 +45,8 @@ export default function LoginPage() {
                 <div className='border-8 border-red-500 h-28 w-96 rounded-xl'></div>
               </div>
               <BigButton displayText='Login'
-                onClick={() => {
-                  setIsLoading(true);
-                  handleUserLogin(username, password);
-
-                }} />
+                onClick={loginFunction.handleSubmit
+                } />
             </div>
           </div>
         </div>
---
+++ src/hooks/useLogin.ts
@@ -0,0 +1,44 @@
+'use client'
+import { useState } from "react"
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
+import { useRouter } from "next/navigation";
+import app from "@/firebase";
+export default function useLogin() {
+
+  const auth = getAuth(app);
+
+  const router = useRouter();
+  const [username, setUsername] = useState("")
+  const [password, setPassword] = useState("")
+  const [isLoading, setIsLoading] = useState(false)
+  const [isError, setIsError] = useState(false)
+
+  const handleUserLogin = async () => {
+    try {
+      await signInWithEmailAndPassword(auth, username, password)
+
+      setIsLoading(false)
+
+      router.push("/educator/dashboard");
+    }
+    catch (e) {
+      setIsLoading(false)
+      setIsError(true)
+    }
+  }
+  const handleUsernameChange = (event: React.FormEvent<HTMLInputElement>) => {
+    setIsError(false)
+    setUsername(event.currentTarget.value)
+
+  }
+  const handlePasswordChange = (event: React.FormEvent<HTMLInputElement>) => {
+    setPassword(event.currentTarget.value)
+    setIsError(false)
+  }
+  const handleSubmit = () => {
+
+    setIsLoading(true);
+    handleUserLogin();
+  }
+  return { loginFunction: { handleSubmit,handleUsernameChange, handlePasswordChange, handleUserLogin }, loginState: { username, password, isLoading, isError } }
+}
