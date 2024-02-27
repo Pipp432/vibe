dfb09781aff2e87b4fd340e43c70562a4a0e72f8
Pipp432 <pipp432@gmail.com>
2024-02-27 08:51:35 +0700

    Add pie chart component
    - Add pie chart component
    - Found problem with labeling the data although formatter is provided

--- src/app/educator/dashboard/page.tsx
+++ src/app/educator/dashboard/page.tsx
@@ -1,11 +1,60 @@
 'use client'
 import Button from "@/components/general/Button"
 
-import CardWrapper from"../../../components/general/CardWrapper"
-import LoadingPage from "@/components/loading/LoadingPage"
-export default function DashboardPage(){
-  return(
+import Chart from "chart.js/auto";
+import { CategoryScale } from "chart.js";
+import ChartDataLabels from 'chartjs-plugin-datalabels';
+import { useState } from "react";
+import { Data } from "@/data/data";
+import PieChart from "@/components/graph/PieChart"
+import CardWrapper from "../../../components/general/CardWrapper"
+import LoadingPage from "@/components/loading/LoadingPage";
+var options = {
+  tooltips: {
+    enabled: false
+  },
+  plugins: {
+    datalabels: {
+      formatter: (value: any, ctx: any) => {
+        let sum = 0;
+        let dataArr = ctx.chart.data.datasets[0].data;
+        dataArr.map((data: any) => {
+          sum += parseInt(data);
+        });
+        let percentage = (value * 100 / sum).toFixed(2) + "%";
+        return percentage;
+      },
+      color: '#fff',
+    }
+  }
+};
+export default function DashboardPage() {
+  Chart.register(CategoryScale);
+  Chart.register(ChartDataLabels);
+  const [chartData, setChartData] = useState({
+    labels: Data.map((data) => data.year),
+    datasets: [
+      {
+        label: "Users Gained ",
+        data: Data.map((data) => data.userGain),
+        backgroundColor: [
+          "rgba(75,192,192,0.5)",
+          "#50AF95",
+          "#FF0000",
+          "#f3ba2f",
+          "#2a71d0"
+        ],
+        borderColor: "black",
+        borderWidth: 2
+      }
+    ], options: options
+  });
+  return (
     <div>
+      <CardWrapper>
+        <PieChart chartData={chartData} />
+      </CardWrapper>
+
     </div>
   )
 }
