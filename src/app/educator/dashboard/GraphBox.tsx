b957ab0534a16b472192c15396b9a8f7a34b05a3
Pipp432 <pipp432@gmail.com>
2024-03-11 14:15:16 +0700

    add graph box

--- src/app/educator/dashboard/GraphBox.tsx
+++ src/app/educator/dashboard/GraphBox.tsx
@@ -1,9 +1,59 @@
-import React from 'react'
+import React,{useState} from 'react'
+import Chart from 'chart.js/auto'
+import BarChart from '@/components/graph/BarChart'
 import CardWrapper from "@/components/general/CardWrapper"
+import {Data} from '@/data/data'
+import { CategoryScale } from "chart.js";
+import ChartDataLabels from 'chartjs-plugin-datalabels';
+
+const options = {
+  tooltips: {
+    enabled: false
+  },
+  plugins: {
+    datalabels: {
+      formatter: (value: any, ctx: any) => {
+        let sum = 0;
+        let dataArr = ctx.chart.data.datasets[0].data;
+        dataArr.map((data: any) => {
+          sum += parseInt(data);
+        });
+        let percentage = (value * 100 / sum).toFixed(2) + "%";
+        return percentage;
+      },
+      color: '#fff',
+    }
+  }
+};
 function GraphBox() {
+  
+  Chart.register(CategoryScale);
+  Chart.register(ChartDataLabels);
+  const [chartData, setChartData] = useState({
+    labels: Data.map((data:any) => data.year),
+    datasets: [
+      {
+        label: "Users Gained ",
+        data: Data.map((data:any) => data.userGain),
+        backgroundColor: [
+          "rgba(75,192,192,0.5)",
+          "#50AF95",
+          "#FF0000",
+          "#f3ba2f",
+          "#2a71d0"
+        ],
+        borderColor: "black",
+        borderWidth: 2
+      }
+    ], options: options
+  });
   return (
   <CardWrapper>
-      <div className='w-[42vw]'></div>
+      <div className='w-[42vw] flex flex-col justify-between h-[100%]'>
+        <BarChart chartData={chartData}/>
+
+      <div className='flex flex-row justify-end'><button className='w-48 h-20 bg-sky-500 text-white rounded-full'>Change Graph</button></div>
+      </div>
     </CardWrapper>
   )
 }
--- src/app/educator/dashboard/page.tsx
+++ src/app/educator/dashboard/page.tsx
@@ -1,59 +1,15 @@
 'use client'
 
 import GraphBox from "./GraphBox";
-import Chart from "chart.js/auto";
 import useModal from "@/hooks/useModal";
-import { CategoryScale } from "chart.js";
-import ChartDataLabels from 'chartjs-plugin-datalabels';
-import { useState } from "react";
-import { Data } from "@/data/data";
 import AnalysisProgress from "./AnalysisProgress";
 import EmotionSummary from "./EmotionSummary";
 import AnalysisProgressPopUp from "@/components/dashboard/AnalysisProgressPopUp";
 import EmotionalSummaryPopUp from "@/components/dashboard/EmotionalSummaryPopup";
 
-const options = {
-  tooltips: {
-    enabled: false
-  },
-  plugins: {
-    datalabels: {
-      formatter: (value: any, ctx: any) => {
-        let sum = 0;
-        let dataArr = ctx.chart.data.datasets[0].data;
-        dataArr.map((data: any) => {
-          sum += parseInt(data);
-        });
-        let percentage = (value * 100 / sum).toFixed(2) + "%";
-        return percentage;
-      },
-      color: '#fff',
-    }
-  }
-};
 export default function DashboardPage() {
   const [isAnalysisModalOpen, toggleAnalysisModal] = useModal()
   const [isEmotionModalOpen, toggleEmotionModal] = useModal()
-  Chart.register(CategoryScale);
-  Chart.register(ChartDataLabels);
-  const [chartData, setChartData] = useState({
-    labels: Data.map((data) => data.year),
-    datasets: [
-      {
-        label: "Users Gained ",
-        data: Data.map((data) => data.userGain),
-        backgroundColor: [
-          "rgba(75,192,192,0.5)",
-          "#50AF95",
-          "#FF0000",
-          "#f3ba2f",
-          "#2a71d0"
-        ],
-        borderColor: "black",
-        borderWidth: 2
-      }
-    ], options: options
-  });
   return (
     <div className="flex gap-10 pt-32 pl-20 justify-center">
       <div className="flex flex-col gap-10">
