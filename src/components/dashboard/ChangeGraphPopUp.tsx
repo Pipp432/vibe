0d901d7e8dd3f399a7eaa838aee1aca3f6a764c6
Pipp432 <pipp432@gmail.com>
2024-03-11 15:33:11 +0700

    Fix overlay modal dimension constraints

--- src/components/dashboard/ChangeGraphPopUp.tsx
+++ src/components/dashboard/ChangeGraphPopUp.tsx
@@ -4,10 +4,12 @@ import OverlayModal from '../general/OverlayModal'
 function ChangeGraphPopUp({closeOverlayHandler}:{closeOverlayHandler:()=>void}) {
   return (
  
-    <OverlayModal title={"Analysis Progress"} closeOverlayHandler={closeOverlayHandler}>
-      <div className='text-sm text-center'>{"On average it takes xx minutes to process the evaluations. File size and number of files uploaded to the system can effect processing time"}</div>
-    </OverlayModal>
- )
+    <OverlayModal title={""} closeOverlayHandler={closeOverlayHandler}>
+      <div className='w-[50rem] h-[30rem]'>
+      
+      </div>
+      </OverlayModal>
+      )
 }
 
 export default ChangeGraphPopUp
--- src/components/general/Modal.tsx
+++ src/components/general/Modal.tsx
@@ -4,7 +4,7 @@ import React from 'react'
 
 function Modal({title,children}:{title:string, children:React.ReactNode}) {
   return (
-    <div className='rounded-lg w-[30rem] h-96 p-4 bg-white'> 
+    <div className='rounded-lg   p-4 bg-white'> 
       <div className='text-2xl text-left text-black'>{title}</div>
       {children}
     </div>
