b48fcf0893255de90a029c2954a1ca441d5cb36e
Pipp432 <pipp432@gmail.com>
2024-03-12 14:16:04 +0700

    Fix padding for modal component

--- src/components/general/Modal.tsx
+++ src/components/general/Modal.tsx
@@ -5,7 +5,7 @@ import React from 'react'
 function Modal({title,children}:{title:string, children:React.ReactNode}) {
   return (
     <div className='rounded-lg   bg-white'> 
-      <div className='text-2xl text-left text-black'>{title}</div>
+      <div className='pt-4 px-4 text-2xl text-left text-black'>{title}</div>
       {children}
     </div>
   )
