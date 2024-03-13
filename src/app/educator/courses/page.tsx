2b25f0da26b534a4c4a320a413ec10bd8f03c3fd
Pipp432 <pipp432@gmail.com>
2024-03-13 11:09:02 +0700

    Add table

--- src/app/educator/courses/page.tsx
+++ src/app/educator/courses/page.tsx
@@ -2,7 +2,25 @@ import React from 'react'
 
 function CoursesPage() {
   return (
-    <div>CoursesPage</div>
+    <div className='grid place-items-center h-full w-full ml-4'>
+      <table className='w-[80vw] h-[30vh] shadow-black shadow-md table-fixed'>
+        <tr className='bg-primary text-white text-lg'>
+          <th>Course ID</th>
+          <th>Course Name</th>
+          <th>Semester</th>
+          <th>Section</th>
+          <th>Data</th>
+        </tr>
+        <tr className=''>
+          <td className='text-center'>Alfreds Futterkiste</td>
+          <td>Maria Anders</td>
+          <td>Maria Anders</td>
+          <td>Germany</td>
+          <td>Germany</td>
+        </tr>
+      </table>
+
+    </div>
   )
 }
 
