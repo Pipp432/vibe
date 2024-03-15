e7d2aaa8808a64c6c7a612a83d5b6de35b084b1b
Pipp432 <pipp432@gmail.com>
2024-03-15 16:07:07 +0700

    Save progress on analysis page

--- src/app/educator/courses/page.tsx
+++ src/app/educator/courses/page.tsx
@@ -4,13 +4,32 @@ function CoursesPage() {
   return (
     <div className='grid place-items-center h-full w-full ml-4'>
       <table className='w-[80vw] h-[30vh] shadow-black shadow-md table-fixed'>
-       <tbody> <tr className='bg-primary text-white text-lg'>
-          <th>Course ID</th>
-          <th>Course Name</th>
-          <th>Semester</th>
-          <th>Section</th>
-          <th>Data</th>
-        </tr></tbody>
+        <tbody>
+          <tr className='bg-primary text-white text-lg'>
+            <th>Course ID</th>
+            <th>Course Name</th>
+            <th className='w-10'>Semester</th>
+            <th>Section</th>
+            <th>Data</th>
+          </tr>
+          <tr className='text-center'>
+            <td>
+              211213
+            </td>
+            <td>
+              OPT APP THEO
+            </td>
+
+            <td>
+              2023/1
+            </td>
+            <td>
+              1
+            </td>
+            <td> N/A</td>
+          </tr>
+
+        </tbody>
       </table>
 
     </div>
