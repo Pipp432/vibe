09f7afe192fe53b8dd174999356b18ff2e7d09ea
Pipp432 <pipp432@gmail.com>
2024-03-20 20:24:38 +0700

    Add confirmation popup

--- src/app/educator/settings/account/page.tsx
+++ src/app/educator/settings/account/page.tsx
@@ -1,36 +1,43 @@
+'use client'
 import AccountItem from '@/components/settings/AccountItem'
 import React from 'react'
 import WarningText from './WarningText'
+import useModal from '@/hooks/useModal'
+import AccountDeletionConfirmationPopUp from '@/components/settings/AccountDeletionConfirmationPopUp'
 
 function AccountSettingsPage() {
+  const [isConfirmationModalOpen, toggleOpenModal] = useModal()
   return (
-    <div className='flex flex-col gap-10 mx-4'>
-      <div className='flex mt-32 '>
-        <div className='flex flex-col'>
-          <div className='text-3xl'>Profile</div>
+    <>
+      {isConfirmationModalOpen && <AccountDeletionConfirmationPopUp closeOverlayHandler={toggleOpenModal} />}
+      <div className='flex flex-col gap-10 mx-4'>
+        <div className='flex mt-32 '>
           <div className='flex flex-col'>
-            <div className='text-lg '>Photo</div>
-            <div className="bg-black w-32 h-32 rounded-full"></div>
+            <div className='text-3xl'>Profile</div>
+            <div className='flex flex-col'>
+              <div className='text-lg '>Photo</div>
+              <div className="bg-black w-32 h-32 rounded-full"></div>
+            </div>
           </div>
-        </div>
-        <div className='flex mt-[10vh] ml-[10vw] gap-x-48'>
-          <div className=' flex flex-col item-start  gap-y-10'>
-            <AccountItem title='Firstname' info='Johnathan' />
-            <AccountItem title='Contact Email' info='HelloWorld@gmail.com' />
-            <AccountItem title='University' info='Chulalongkorn University' />
-            <AccountItem title='Department' info='Electrical Engineering' />
-          </div>
-          <div className=' flex flex-col item-start gap-x-72 gap-y-10'>
-            <AccountItem title='Surname' info='Smithsonian' />
-            <AccountItem title='Phone Number' info='+66892245258' />
+          <div className='flex mt-[10vh] ml-[10vw] gap-x-48'>
+            <div className=' flex flex-col item-start  gap-y-10'>
+              <AccountItem title='Firstname' info='Johnathan' />
+              <AccountItem title='Contact Email' info='HelloWorld@gmail.com' />
+              <AccountItem title='University' info='Chulalongkorn University' />
+              <AccountItem title='Department' info='Electrical Engineering' />
+            </div>
+            <div className=' flex flex-col item-start gap-x-72 gap-y-10'>
+              <AccountItem title='Surname' info='Smithsonian' />
+              <AccountItem title='Phone Number' info='+66892245258' />
 
-            <AccountItem title='Faculty' info='Engineering' />
-         <button className='h-10 w-52 bg-secondary rounded-full text-lg'>Edit Profile</button>
+              <AccountItem title='Faculty' info='Engineering' />
+              <button className='h-10 w-52 bg-secondary rounded-full text-lg'>Edit Profile</button>
+            </div>
           </div>
         </div>
+        <WarningText onClick={toggleOpenModal} />
       </div>
-      <WarningText onClick={handleOpenConfirmationModal}/>
-    </div>
+    </>
   )
 }
 
---
+++ src/components/settings/AccountDeletionConfirmationPopUp.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import OverlayModal from '../general/OverlayModal'
+function AccountDeletionConfirmationPopUp({closeOverlayHandler}:{closeOverlayHandler:()=>void}) {
+  return (
+    <OverlayModal title={"Confirmation"} closeOverlayHandler={closeOverlayHandler}>
+      <div className='text-sm text-center w-[30rem] h-96 p-4'>{"On average it takes xx minutes to process the evaluations. File size and number of files uploaded to the system can effect processing time"}</div>
+      
+    </OverlayModal>
+  )
+}
+
+export default AccountDeletionConfirmationPopUp
