670c1d58ebbafe16ce6dfb2cb7451fc3b5b2c28b
Pipp432 <pipp432@gmail.com>
2024-03-23 01:53:56 +0700

    Fix indicator and add animation

--- src/components/navigation/NavigationTab.tsx
+++ src/components/navigation/NavigationTab.tsx
@@ -6,6 +6,7 @@ import logo from "../../../public/images/logo.png"
 import SelectionIndicator from './SelectionIndicator'
 import { usePathname, useRouter } from 'next/navigation'
 function NavigationTab() {
+  const [position,setPosition] = useState(28);
   const [currentSelection, setCurrentSelection] = useState(0);
   const router = useRouter();
   const pathname = usePathname();
@@ -13,7 +14,11 @@ function NavigationTab() {
 
   const changeIndicatorPosition = (changePosition: number) => {
 
+    if(currentSelection>changePosition) setPosition(28-16.5*(changePosition))
+    if(currentSelection<changePosition)setPosition(28+16.5*(changePosition))
+
     setCurrentSelection(changePosition)
+    
   }
   const pathDict: any = {
     "dashboard": 0,
@@ -21,54 +26,43 @@ function NavigationTab() {
     "courses": 2,
     "settings": 3
   }
-  useEffect(() => {
-    changeIndicatorPosition(pathDict[pagename])
-  })
+  //useEffect(() => {
+  //  changeIndicatorPosition(pathDict[pagename])
+ // })
   return (
+    <>
     <div className='w-72 h-full bg-primary relative'>
-      <div className='translate-y-64'>
 
-      </div>
-      <div className='cursor-pointer my-4 flex flex-col gap-20 items-center text-2xl'>
+      <div className='absolute top-0 duration-200'style={{transform:`translateY(${position}vh)`}}><SelectionIndicator/></div>
+      <div className='cursor-pointer my-4 flex flex-col justify-center gap-[10vh] items-center text-2xl'>
 
         <Image src={logo} width={160} height={60} alt='logo' />
-        <div className='flex flex-row w-full justify-between'>
-          {currentSelection === 0 ? <SelectionIndicator /> : <div />}
           <NavigationItem name={"Dashboard"} onClick={() => {
             router.push("/educator/dashboard")
 
             changeIndicatorPosition(0)
           }} />
-        </div>
-        <div className='flex flex-row w-full justify-between'>
-          {currentSelection === 1 ? <SelectionIndicator /> : <div />}
           <NavigationItem name={"Analysis"} onClick={() => {
             changeIndicatorPosition(1)
             router.push("/educator/analysis")
           }} />
-        </div>
-        <div className='flex flex-row w-full justify-between'>
 
-          {currentSelection === 2 ? <SelectionIndicator /> : <div />}
           <NavigationItem name={"Courses"} onClick={() => {
 
             router.push("/educator/courses")
             changeIndicatorPosition(2)
           }} />
-        </div>
-        <div className='flex flex-row w-full justify-between'>
 
-          {currentSelection === 3 ? <SelectionIndicator /> : <div />}
           <NavigationItem name={"Settings"} onClick={() => {
             changeIndicatorPosition(3)
             router.push("/educator/settings/account")
           }
           } />
-        </div>
 
       </div>
 
     </div>
+  </>
   )
 }
 
