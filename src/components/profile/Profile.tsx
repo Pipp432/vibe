89ac174b57c081473497da2ca0d2366e35a1a749
Pipp432 <pipp432@gmail.com>
2024-03-23 14:49:41 +0700

    Add hanlding for no login user

--- src/components/profile/Profile.tsx
+++ src/components/profile/Profile.tsx
@@ -1,20 +1,33 @@
 'use client'
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { IoMdArrowDropdown } from "react-icons/io";
 import ProfileMenu from "@/components/profile/ProfileMenu"
+import { getAuth, signOut } from 'firebase/auth';
 function Profile() {
   const [isOpen, setIsOpen] = useState(false)
+  const [isLoggedIn, setIsLoggedIn] = useState(false)
   const handleToggleMenu = () => {
     setIsOpen(!isOpen)
 
   }
+  useEffect(()=>{
+
+  try {
+    
+      const auth = getAuth();
+      if(auth) setIsLoggedIn(true)
+  } catch (error) {
+   setIsLoggedIn(false) 
+  }
+  },[])
+  
   return (
     <div className='flex flex-col items-end p-4'>
       <div className=' justify-between w-[25vw] h-24 rounded-lg flex items-center'>
         <div className='rounded-full bg-black h-20 w-20'></div>
         <div className='flex flex-col'>
-          <div className='text-xl'>{"Johnathan Smith"}</div>
-          <div className='text-lg'>{"Course Director"}</div>
+          <div className='text-xl'>{isLoggedIn?"Johnathan Smith":"Please login"}</div>
+          {isLoggedIn && <div className='text-lg'>{"Course Director"}</div>}
 
         </div>
         <button className="" onClick={handleToggleMenu}>
