aa523691087d9574e23035857008f90b324a43cb
Pipp432 <pipp432@gmail.com>
2024-03-23 16:22:40 +0700

    Fix reload bug

--- src/components/settings/SettingsNavigation.tsx
+++ src/components/settings/SettingsNavigation.tsx
@@ -1,35 +1,38 @@
 'use client'
-import React ,{useRef,useState}from 'react'
+import React ,{useRef,useState,useEffect}from 'react'
 import SelectionIndicator from '../navigation/SelectionIndicator'
 
-import { useRouter } from 'next/navigation'
+import { usePathname, useRouter } from 'next/navigation'
 export const SettingsNavigation = () => {
   
   const indicator = useRef<HTMLDivElement>(null);
   const router =  useRouter(); 
   const [currentSelection, setCurrentSelection] = useState(0);
+  const [position,setPosition] = useState(10)
   const changeIndicatorPosition = (changePosition: number) => {
 
-    if (indicator.current) {
-      console.log(changePosition);
-      if (currentSelection < changePosition) {
-        indicator.current.style.top = parseFloat(indicator.current.style.top.replace("rem", "")) +
-          (7.5 * (changePosition - currentSelection)) + "rem"
-        setCurrentSelection(changePosition)
-      }
-      if (currentSelection > changePosition) {
-
-        indicator.current.style.top = parseFloat(indicator.current.style.top.replace("rem", ""))
-          + (7.5 * (changePosition - currentSelection)) + "rem"
-        setCurrentSelection(changePosition)
-      }
-    }
-
+    if(currentSelection>changePosition) setPosition(position - 11.5*(currentSelection-changePosition))
+    if(currentSelection<changePosition)setPosition(position +11.5*(changePosition-currentSelection))
+    console.log(currentSelection,changePosition);
+    console.log(position)
+    
+    setCurrentSelection(changePosition)
   }
+
+  const pathDict: any = {
+    "account":0,
+    "information":1
+  }
+  const pathname = usePathname();
+  const pagename: string = pathname.split("/").pop()!
+  useEffect(() => {
+    console.log(pagename)
+    changeIndicatorPosition(pathDict[pagename])
+   })
   return (
     <div className='relative h-[100vh] bg-subTab w-48'>
-      <div className='translate-y-24'>
-        <SelectionIndicator reference={indicator}/>
+      <div className='absolute duration-200'style={{transform:`translateY(${position}vh)`}}>
+        <SelectionIndicator />
       </div>
       <div className='p-10 justify-center items-center flex flex-col gap-20 text-2xl text-white'>
         <div className='mt-2'></div>
