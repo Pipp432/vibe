a7daef2cb0e5ac1d8ae11d2f176a224e64bcfe4e
Pipp432 <pipp432@gmail.com>
2024-04-02 22:15:27 +0700

    Add details and functionality to score popup

--- src/components/analysis/OverallScorePopup.tsx
+++ src/components/analysis/OverallScorePopup.tsx
@@ -1,9 +1,33 @@
 import React from 'react'
 import OverlayModal from '@/components/general/OverlayModal'
-function OverallScorePopup({closeOverlayHandler}:{closeOverlayHandler:()=>void}) {
+function OverallScorePopup({ closeOverlayHandler }: { closeOverlayHandler: () => void }) {
   return (
-    <OverlayModal title={"Analysis Progress"} closeOverlayHandler={closeOverlayHandler}>
-      <div className='w-[30rem] h-96 text-sm text-center p-4'>{"On average it takes xx minutes to process the evaluations. File size and number of files uploaded to the system can effect processing time"}</div>
+    <OverlayModal title={"Emotional Score"} closeOverlayHandler={closeOverlayHandler}>
+      <div className='flex flex-col h-96 gap-4 p-4'>
+        <div className='w-[30rem] text-sm'>
+          {"Emotional score provides a general numerical value to interpret. The higher the value the more positive the students feel towards the course. The score is calulcated by aggregating all emotions using the PAD model."}
+        </div>
+        <div className='grid grid-cols-2 gap-x-4 gap-y-2'>
+          <div className='flex gap-4 items-center'>
+            <div className='bg-green-500 text-white p-2 text-center w-20'>100-81</div>
+            <div>Very positive</div>
+          </div>
+          <div className='flex gap-4 items-center'>
+            <div className='bg-yellow-300 text-white p-2 text-center w-20'>80-61</div>
+            <div>Positive</div>
+          </div>
+          <div className='flex gap-4 items-center'>
+            <div className='bg-orange-500 text-white p-2 text-center w-20'>60-41</div>
+            <div>Negative</div>
+          </div>
+          <div className='flex gap-4 items-center'>
+            <div className='bg-red-500 text-white p-2 text-center w-20'>{"<40"}</div>
+            <div>Very negative</div>
+          </div>
+        </div>
+
+      </div>
+
     </OverlayModal>
   )
 }
