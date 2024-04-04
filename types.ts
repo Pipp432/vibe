c6ade7ff6ee41cf6bbcf5c7f6b6ba38fcc4f45e4
Pipp432 <pipp432@gmail.com>
2024-04-04 10:53:44 +0700

    Add types regarding the context segmentatio

--- types.ts
+++ types.ts
@@ -1,33 +1,56 @@
-export type CourseType ={
-  courseID:number;
-  section:number ;
-  semester:number ;
-  year:string;
-  name:string ;
-  major:string ;
-  faculty:string ;
+export type CourseType = {
+  courseID: string;
+  section: number;
+  semester: string;
+  year: string;
+  name: string;
+  major: string;
+  faculty: string;
+  isUpload: number;
 }
-export type ProfessorType={
-  emailChula:string; 
-  firstName:string;
-  lastName:string;
-  role:string;
+export type ProfessorType = {
+  emailChula: string;
+  firstName: string;
+  lastName: string;
+  role: string;
 }
-export type CommentType={
-  commentID:number;
+export type CommentType = {
+  commentID: number;
   text: string,
   courseID: number,
-  section:number ,
-  semester:number ,
-  year:string,
+  section: number,
+  semester: number,
+  year: string,
 
 }
-export type CommentContextType={
+export type CommentContextType = {
   contextCommentID: number;
-  commentID:number;
-  text:string,
-  context :string;
-  contextComment: string,
-  emotionScore: number,
+  commentID: number;
+  text: string,
+  context: string;
+  contextComment: string;
+  emotionScore: number;
 
+}
+export type EmotionObjectType = {
+  "approval": number;
+  "disapproval": number;
+  "confusion": number;
+  "joy": number;
+  "neutrality": number;
+  "graditude": number;
+  "disappointment": number;
+  "anger": number;
+  "admiration": number;
+}
+export type ContextEmotionObjectType = {
+  "Teaching": EmotionObjectType;
+  "Assessment": EmotionObjectType;
+  "Teaching Material": EmotionObjectType;
+  "Facility": EmotionObjectType;
+  "None": EmotionObjectType;
+
+}
+export type ContextType = {
+  "context":ContextEmotionObjectType; 
 }
