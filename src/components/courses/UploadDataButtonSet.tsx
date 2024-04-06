38d98e1b06cface92a61bf18ad320ef2d7cdb278
Pipp432 <pipp432@gmail.com>
2024-04-06 22:38:05 +0700

    handle file upload logic

--- src/components/courses/UploadDataButtonSet.tsx
+++ src/components/courses/UploadDataButtonSet.tsx
@@ -1,21 +1,31 @@
 'use client'
-import React, { useState } from 'react'
+import React, { useRef } from 'react'
 
 function UploadDataButtonSet() {
-  const [file, setFile] = useState<File>();
-  const handleFileUpload = (e: React.FormEvent<HTMLInputElement>) => {
-    if (e.currentTarget.files) {
-      setFile(e.currentTarget.files[0])
-      console.log(e.currentTarget.files[0].name)
+  const formElement = useRef(null);
+  const sendFileToML = async (e: any) => {
+    e.preventDefault();
+    try {
+      if (formElement.current) {
+        const data = new FormData(formElement.current)
+        console.log("Run")
+        const result = await fetch("http://127.0.0.1:5000/test_csv", { method: "POST", body: data})
+      }
+    }
+    catch {
+      console.log("Error")
     }
   }
   return (
     <div className='flex flex-row gap-10 items-center justify-end'>
-      <div className='text-black text-lg'>No Data Found</div>
-      <label className='w-52 p-2 text-white rounded-full bg-success cursor-pointer' htmlFor='file-upload'>
-        {"Upload"}
-      </label>
-      <input id='file-upload' className='hidden' onChange={handleFileUpload} type='file'></input>
+      <form method="POST" className='w-full flex gap-2 justify-center items-center' ref={formElement} onSubmit={sendFileToML} action="" encType="multipart/form-data">
+        <div className='text-black text-lg'>No Data Found</div>
+        <label className='w-52 p-2 text-white rounded-full bg-success cursor-pointer' htmlFor='file-upload'>
+          {"Upload"}
+        </label>
+        <input id='file-upload' name='file' className='hidden' type='file'></input>
+        <button type='submit'>Test</button>
+      </form>
     </div>
 
   )
