14d9a797cb7db7b94739e736ebe53706c5f1eaab
Pipp432 <pipp432@gmail.com>
2024-04-06 11:20:38 +0700

    added file upload logic to courses table

--- src/components/courses/UploadDataButtonSet.tsx
+++ src/components/courses/UploadDataButtonSet.tsx
@@ -1,15 +1,21 @@
-import React from 'react'
+'use client'
+import React, { useState } from 'react'
 
 function UploadDataButtonSet() {
+  const [file, setFile] = useState<File>();
+  const handleFileUpload = (e: React.FormEvent<HTMLInputElement>) => {
+    if (e.currentTarget.files) {
+      setFile(e.currentTarget.files[0])
+      console.log(e.currentTarget.files[0].name)
+    }
+  }
   return (
     <div className='flex flex-row gap-10 items-center justify-end'>
       <div className='text-black text-lg'>No Data Found</div>
       <label className='w-52 p-2 text-white rounded-full bg-success cursor-pointer' htmlFor='file-upload'>
-
-      Upload
+        {"Upload"}
       </label>
-
-        <input id ='file-upload'className='hidden'  type='file'></input>
+      <input id='file-upload' className='hidden' onChange={handleFileUpload} type='file'></input>
     </div>
 
   )
