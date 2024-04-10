b6c190f8aef0fbbf0a1f1a4404c093d8709d0d2e
Pipp432 <pipp432@gmail.com>
2024-04-10 11:13:28 +0700

    add emoji icosn

--- src/components/graph/EmotionRankChart.tsx
+++ src/components/graph/EmotionRankChart.tsx
@@ -1,14 +1,14 @@
 import React from 'react'
-
-function EmotionRankChart({ percentage, course }: { percentage: Array<any>, course?: String }) {
+import TextToEmojiMap from '@/data/TextToEmojiMap'
+function EmotionRankChart({ data, course }: { data: any, course?: String }) {
   return (
     <div className='flex flex-row gap-10 justify-between '>
       {course && <div className='ml-10 text-xl'>{course}</div>}
-      <div className='flex-row flex w-[25vw] h-6 justify-end'>
-        <div className='rounded-tl-full rounded-bl-full bg-blue-800 '
-          style={{ width: percentage[0] }}></div>
-        <div style={{ width: percentage[1] }} className='bg-sky-600 '></div>
-        <div style={{ width: percentage[2] }} className='bg-slate-600   rounded-tr-full rounded-br-full'></div>
+      <div className='flex-row flex w-[25vw] h-8 justify-end'>
+        <div className='rounded-tl-full rounded-bl-full bg-blue-800 flex justify-center p-1 '
+          style={{ width: "60%" }}><TextToEmojiMap text="disapproval"/></div>
+        <div style={{ width: "20%" }} className='bg-sky-600 flex justify-center p-1'><TextToEmojiMap text='joy'/></div>
+        <div style={{ width: "20%" }} className='bg-slate-600  flex justify-center p-1 rounded-tr-full rounded-br-full'><TextToEmojiMap text='gratitude'/></div>
 
       </div>
 
---
+++ src/data/TextToEmojiMap.tsx
@@ -0,0 +1,26 @@
+import Image from "next/image";
+const textToEmojiMap = ({ text }: { text: string }) => {
+  const height = "24"
+  const width = "24"
+  switch (text) {
+    case "joy":
+      return <Image alt={"joy"} height={height} width={width} src="/images/joy.jpg" />
+    case "neutrality":
+      return <Image height={height} width={width} alt="neutrality" src="/images/neutrality.jpg" />
+    case "anger":
+      return <Image alt={"anger"} height={height} width={width} src="/images/anger.jpg" />
+    case "disapproval":
+      return <Image height={"25"} width={"25"} alt="disapproval" src="/images/disapproval.jpg" />
+    case "disappointment":
+      return <Image alt={"disappointment"} height={height} width={width} src="/images/disappointment.jpg" />
+    case "approval":
+      return <Image height={height} width={width} alt="approval" src="/images/approval.jpg" />
+    case "gratitude":
+      return <Image alt={"graditude"} height={height} width={width} src="/images/gratitude.jpg" />
+    case "admiration":
+      return <Image height={height} width={width} alt="admiration" src="/images/admiration.jpg" />
+    default:
+      break;
+  }
+}
+export default textToEmojiMap
