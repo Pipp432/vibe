0c6079ad4cb00f91e88ac1de7a14e7005553ac83
Pipp432 <pipp432@gmail.com>
2024-04-11 09:18:55 +0700

    general fix

--- src/components/graph/EmotionRankChartPopUp.tsx
+++ src/components/graph/EmotionRankChartPopUp.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function EmotionRankChartPopUp({emotion}:{emotion:string}) {
   return (
-    <div className='z-10 bg-white border-2 border-black shadow-black shadow-md p-2 rounded-lg'>{emotion}</div>
+    <div className='z-10 bg-white border-2 border-black shadow-black shadow-md p-2 rounded-lg'>{emotion[0].toLocaleUpperCase() + emotion.substring(1,emotion.length)}</div>
   )
 }
 
