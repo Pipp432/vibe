bf464b07bcf7f97b434003354de638a1fb4c1273
Pipp432 <pipp432@gmail.com>
2024-04-16 22:24:13 +0700

    Add button to redirect into login

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,11 @@
-export default async function Home() {
-  return (
-    <div className='bg-black p-10 text-white'>
+"use client"
+import { useRouter } from "next/navigation"
+
+export default function Home() {
+const router = useRouter()  
+  return(
+    <div className='grid place-items-center h-[100vh]'>
+      <button className='p-2 bg-secondary text-black text-3xl rounded-lg'onClick={()=>{router.push("/login")}}>ENTER VIBE</button>
     </div>
   );
 }
