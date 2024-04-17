97b07e1fdfc044f009292194c735aed0e0be22c4
Pipp432 <pipp432@gmail.com>
2024-04-17 10:24:30 +0700

    hot fix

--- src/components/profile/Profile.tsx
+++ src/components/profile/Profile.tsx
@@ -1,5 +1,5 @@
 'use client'
-import React, { useContext, useEffect, useReducer, useState } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import { IoMdArrowDropdown } from "react-icons/io";
 import ProfileMenu from "@/components/profile/ProfileMenu"
 import { getAuth } from 'firebase/auth';
@@ -7,15 +7,24 @@ import { LoginContext } from '@/app/contexts/LoginContext';
 import { ProfessorType } from '../../../types';
 function Profile() {
   
-  const loginState:ProfessorType = useContext(LoginContext)
-  const currentUser = localStorage.getItem('emailChula')||""
-  console.log(currentUser.length!==0)
   const [isOpen, setIsOpen] = useState(false)
-  const [isLoggedIn, setIsLoggedIn] = useState(currentUser.length!==0)
+  const [isLoggedIn, setIsLoggedIn] = useState(false)
   const handleToggleMenu = () => {
     setIsOpen(!isOpen)
 
   }
+  useEffect(() => {
+
+    try {
+
+      const auth = getAuth();
+      if (auth) setIsLoggedIn(true)
+    } catch (error) {
+      setIsLoggedIn(false)
+    }
+  }, [])
+  const loginState:ProfessorType = useContext(LoginContext)
+  console.log(loginState)
   return (
     <div className='flex flex-col items-end p-4'>
       <div className=' justify-between w-[25vw] h-24 rounded-lg flex items-center'>
