2302c8654f4c33c64e5d89ab31d2ed1283c6e5d8
Pipp432 <pipp432@gmail.com>
2024-04-17 10:22:13 +0700

    Fix

--- src/app/contexts/LoginContext.tsx
+++ src/app/contexts/LoginContext.tsx
@@ -1,26 +1,27 @@
 'use client'
-import { useEffect,Dispatch, ReactNode, Reducer, createContext, useReducer } from 'react';
+import { useEffect, Dispatch, ReactNode, Reducer, createContext, useReducer } from 'react';
 import { LoginDispatchType, ProfessorType } from '../../../types';
 
 export const LoginContext = createContext(null as unknown as ProfessorType);
 export const LoginDispatchContext = createContext<Dispatch<LoginDispatchType>>(null as unknown as Dispatch<LoginDispatchType>);
 export function LoginProvider({ children }: { children: ReactNode }) {
-  const currentUser = {"emailChula":"", "firstName": "John", "lastName": "Doe", "role": "N/A"}
-  const intialLogin:ProfessorType = {
-    emailChula:localStorage.getItem('emailChula')||'',
-    firstName:localStorage.getItem('firstName')||"Joe",
-    lastName:localStorage.getItem('lastName')||'Joe',
-    role:localStorage.getItem('role')||'Joe'
+  const currentUser = { "emailChula": "", "firstName": "John", "lastName": "Doe", "role": "N/A" }
+
+  const intialLogin: ProfessorType = {
+    emailChula: "",
+    firstName: "Joe",
+    lastName: 'Joe',
+    role: 'Joe'
   }
   const loginReducer = (state: ProfessorType, action: LoginDispatchType) => {
     switch (action.type) {
       case 'login':
-        return { ...state, firstName: action.payload.firstName, lastName: action.payload.lastName, role: action.payload.role, emailChula:action.payload.emailChula }
+        return { ...state, firstName: action.payload.firstName, lastName: action.payload.lastName, role: action.payload.role, emailChula: action.payload.emailChula }
       default:
         break;
     }
   }
-  const [loginState, dispatch] = useReducer<Reducer<any,any>>(loginReducer, intialLogin)
+  const [loginState, dispatch] = useReducer<Reducer<any, any>>(loginReducer, intialLogin)
 
   return (
     <LoginContext.Provider value={loginState}>
